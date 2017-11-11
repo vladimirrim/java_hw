4510e1dc8320facad40e6d18f13dde6e40e491ca
vladimirrim <vladimirrim98@gmail.com>
2017-11-12 00:23:32 +0300

    Improve directory deletion, change / to File.separator

--- hw_4_ZipFile/src/test/java/ru/spbau/egorov/hw_4/zipfile/ZipFileTest.java
+++ hw_4_ZipFile/src/test/java/ru/spbau/egorov/hw_4/zipfile/ZipFileTest.java
@@ -30,17 +30,17 @@ class ZipFileTest {
 
         for (int i = 0; i < 10; i++) {
             zipFile("___test" + i,
-                    new ZipOutputStream(new FileOutputStream(new File("___test/___test" + i + ".zip"))), "" + i);
+                    new ZipOutputStream(new FileOutputStream(new File("___test" + File.separator + "___test" + i + ".zip"))), "" + i);
         }
 
         ZipFile.unzipFiles("___test", "___(.*)");
 
         for (int i = 0; i < 10; i++) {
-            Scanner scanner = new Scanner(new File("___test/___test" + i));
+            Scanner scanner = new Scanner(new File("___test" + File.separator + "___test" + i));
             assertEquals(i, scanner.nextInt());
             assertFalse(scanner.hasNextInt());
         }
-        FileUtils.deleteDirectory(new File("___test"));
+        FileUtils.deleteDirectory(Paths.get("___test").toFile());
     }
 
 
@@ -49,39 +49,39 @@ class ZipFileTest {
 
         Files.createDirectory(Paths.get("___test"));
 
-        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(new File("___test/___test.zip")));
+        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(new File("___test" + File.separator + "___test.zip")));
 
 
-        zipFile("layer1/layer2/layer3/layer4/___test" + 2, out, "" + 2);
-        zipFile("layer1/layer2/layer3/___test" + 1, out, "" + 1);
-        zipFile("layer1/layer2/___test" + 1, out, "" + 1);
-        zipFile("layer1/___test" + 1, out, "" + 1);
+        zipFile("layer1" + File.separator + "layer2" + File.separator + "layer3" + File.separator + "layer4" + File.separator + "___test" + 2, out, "" + 2);
+        zipFile("layer1" + File.separator + "layer2" + File.separator + "layer3" + File.separator + "___test" + 1, out, "" + 1);
+        zipFile("layer1" + File.separator + "layer2" + File.separator + "___test" + 1, out, "" + 1);
+        zipFile("layer1" + File.separator + "___test" + 1, out, "" + 1);
 
         out.close();
 
         ZipFile.unzipFiles("___test", "___test1");
 
         {
-            Scanner scanner = new Scanner(new File("___test/layer1/___test1"));
+            Scanner scanner = new Scanner(new File("___test" + File.separator + "layer1" + File.separator + "___test1"));
             assertEquals(1, scanner.nextInt());
             assertFalse(scanner.hasNextInt());
         }
 
         {
-            Scanner scanner = new Scanner(new File("___test/layer1/layer2/___test1"));
+            Scanner scanner = new Scanner(new File("___test" + File.separator + "layer1" + File.separator + "layer2" + File.separator + "___test1"));
             assertEquals(1, scanner.nextInt());
             assertFalse(scanner.hasNextInt());
         }
 
         {
-            Scanner scanner = new Scanner(new File("___test/layer1/layer2/layer3/___test1"));
+            Scanner scanner = new Scanner(new File("___test" + File.separator + "layer1" + File.separator + "layer2" + File.separator + "layer3" + File.separator + "___test1"));
             assertEquals(1, scanner.nextInt());
             assertFalse(scanner.hasNextInt());
         }
 
-        assertFalse((new File("___test/layer1/layer2/layer3/layer4/___test2").exists()));
+        assertFalse((new File("___test" + File.separator + "layer1" + File.separator + "layer2" + File.separator + "layer3" + File.separator + "layer4" + File.separator + "___test2").exists()));
 
-        FileUtils.deleteDirectory(new File("___test"));
+        FileUtils.deleteDirectory(Paths.get("___test").toFile());
     }
 
 }

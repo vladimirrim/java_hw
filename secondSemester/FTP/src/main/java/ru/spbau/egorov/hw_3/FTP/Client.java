c6bf0035bb28142b95ee1259d96c014f794b8997
vladimirrim <vladimirrim98@gmail.com>
2018-05-03 21:24:13 +0300

    1.0

---
+++ secondSemester/FTP/src/main/java/ru/spbau/egorov/hw_3/FTP/Client.java
@@ -0,0 +1,91 @@
+package ru.spbau.egorov.hw_3.FTP;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * This class implements list and get requests to FTP server.
+ */
+public class Client {
+    private String hostName;
+    private int port;
+
+    public Client(String hostName, int port) {
+        this.hostName = hostName;
+        this.port = port;
+    }
+
+    /**
+     * Requests all files in the specified directory.The protocol for request: <1:Int><path:String>
+     * @param path is the path to the directory.
+     * @param os is stream where the request result will be written.
+     * @throws InvalidProtocolException if the protocol from server didn`t match with the actual protocol.
+     * @throws IOException if server didn`t respond correctly or the was connection problems.
+     */
+    public void list(String path, OutputStream os) throws InvalidProtocolException, IOException {
+        try (Socket socket = new Socket(hostName, port);
+             DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+             DataOutputStream dos = new DataOutputStream(os)) {
+            outputStream.writeInt(1);
+            outputStream.writeInt(path.getBytes().length);
+            outputStream.write(path.getBytes());
+            int size = inputStream.readInt();
+            if (size < 0)
+                throw new InvalidProtocolException();
+            dos.write("Number of files in directory ".getBytes());
+            dos.write(path.getBytes());
+            dos.write(": ".getBytes());
+            dos.write(Integer.toString(size).getBytes());
+            dos.write(System.getProperty("line.separator").getBytes());
+            for (int i = 0; i < size; i++) {
+                int stringSize = inputStream.readInt();
+                if (stringSize <= 0)
+                    throw new InvalidProtocolException();
+                byte[] buffer = new byte[stringSize];
+                inputStream.readFully(buffer);
+                boolean isDir = inputStream.readBoolean();
+                dos.write("name: ".getBytes());
+                dos.write(buffer);
+                dos.write(" isDirectory: ".getBytes());
+                dos.write(Boolean.toString(isDir).getBytes());
+                dos.write(System.getProperty("line.separator").getBytes());
+            }
+        }
+    }
+
+    /**
+     * Requests specified file content.The protocol for request: <2:Int><path:String>
+     * @param path is the path to the file.
+     * @param os is stream where the request result will be written.
+     * @throws InvalidProtocolException if the answer from server didn`t match with the actual protocol.
+     * @throws IOException if server didn`t respond correctly or the was connection problems.
+     */
+    public void get(String path, OutputStream os) throws InvalidProtocolException, IOException {
+        try (Socket socket = new Socket(hostName, port);
+             DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())) {
+            outputStream.writeInt(2);
+            outputStream.writeInt(path.getBytes().length);
+            outputStream.write(path.getBytes());
+            outputStream.flush();
+            long size = inputStream.readLong();
+            if (size < 0)
+                throw new InvalidProtocolException();
+            byte[] buffer = new byte[1024];
+            if (size != 0) {
+                for (int read = inputStream.read(buffer); read != -1; read = inputStream.read(buffer)) {
+                    size -= read;
+                    if (size < 0)
+                        throw new InvalidProtocolException();
+                    os.write(buffer, 0, read);
+                }
+                if (size > 0)
+                    throw new InvalidProtocolException();
+            }
+        }
+    }
+}
---
+++ secondSemester/FTP/src/test/java/ru/spbau/egorov/hw_3/FTP/ServerTest.java
@@ -0,0 +1,107 @@
+package ru.spbau.egorov.hw_3.FTP;
+
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+
+import static java.lang.Thread.sleep;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ServerTest {
+    private static String hostName;
+    private static Server server;
+    private String lineSeparator = System.getProperty("line.separator");
+
+    @BeforeAll
+    static void setUp() throws InterruptedException {
+        server = new Server(8888);
+        new Thread(server::start).start();
+        sleep(100);
+        hostName = server.getHostName();
+    }
+
+    @Test
+    void onGetRequestEmptyPathNameExceptions() throws IOException, InvalidProtocolException, InterruptedException {
+        Client client = new Client(hostName, 8888);
+        assertThrows(EOFException.class, () -> client.get("", new ByteArrayOutputStream()));
+        sleep(100);
+        assertEquals(InvalidProtocolException.class, server.getExceptionsHistory().get(server.getExceptionsHistory().size() - 1).getClass());
+    }
+
+    @Test
+    void onListRequestEmptyPathNameExceptions() throws IOException, InvalidProtocolException, InterruptedException {
+        Client client = new Client(hostName, 8888);
+        assertThrows(EOFException.class, () -> client.list("", new ByteArrayOutputStream()));
+        sleep(100);
+        assertEquals(InvalidProtocolException.class, server.getExceptionsHistory().get(server.getExceptionsHistory().size() - 1).getClass());
+    }
+
+    @Test
+    void onListRequestNotADirectory() throws IOException, InvalidProtocolException {
+        Client client = new Client(hostName, 8888);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        client.list("./testDirs/file.txt", stream);
+        assertEquals("Number of files in directory ./testDirs/file.txt: 0" + lineSeparator, stream.toString());
+    }
+
+    @Test
+    void onListRequestEmptyDirectory() throws IOException, InvalidProtocolException {
+        Client client = new Client(hostName, 8888);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        client.list("./testDirs/dir1", stream);
+        assertEquals("Number of files in directory ./testDirs/dir1: 0" + lineSeparator, stream.toString());
+    }
+
+    @Test
+    void oneGetAndOneListRequests() throws IOException, InvalidProtocolException {
+        Client client = new Client(hostName, 8888);
+        String ansList = "Number of files in directory ./testDirs: 2" + lineSeparator +
+                "name: dir1 isDirectory: false" + lineSeparator +
+                "name: file.txt isDirectory: false" + lineSeparator;
+        String ansGet = "jojo";
+        ByteArrayOutputStream[] streams = new ByteArrayOutputStream[2];
+        streams[0] = new ByteArrayOutputStream();
+        client.list("./testDirs", streams[0]);
+        streams[1] = new ByteArrayOutputStream();
+        client.get("./testDirs/file.txt", streams[1]);
+        assertEquals(ansGet, streams[1].toString());
+        assertEquals(ansList, streams[0].toString());
+    }
+
+    @Test
+    void onListRequestTenRequests() throws IOException, InterruptedException, InvalidProtocolException {
+        Client client = new Client(hostName, 8888);
+        String lineSeparator = System.getProperty("line.separator");
+        String ans = "Number of files in directory ./testDirs: 2" + lineSeparator +
+                "name: dir1 isDirectory: false" + lineSeparator +
+                "name: file.txt isDirectory: false" + lineSeparator;
+        ByteArrayOutputStream[] streams = new ByteArrayOutputStream[10];
+        for (int i = 0; i < 10; i++) {
+            streams[i] = new ByteArrayOutputStream();
+            client.list("./testDirs", streams[i]);
+        }
+        for (int i = 0; i < 10; i++) {
+            assertEquals(ans, streams[i].toString());
+        }
+    }
+
+    @Test
+    void onGetRequestTenRequests() throws InterruptedException, IOException, InvalidProtocolException {
+        Client client = new Client(hostName, 8888);
+        ByteArrayOutputStream[] streams = new ByteArrayOutputStream[10];
+        String ans = "jojo";
+        for (int i = 0; i < 10; i++) {
+            streams[i] = new ByteArrayOutputStream();
+            client.get("./testDirs/file.txt", streams[i]);
+        }
+        for (int i = 0; i < 10; i++) {
+            assertEquals(ans, streams[i].toString());
+        }
+    }
+
+}
